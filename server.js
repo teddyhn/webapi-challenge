cd4cbc7a01d9e520c0514810d899120b6e61d40f
teddyhn <tngo97@gmail.com>
2019-10-04 08:41:37 -0700

    projectRouter endpoints built

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
+const projectRouter = require('./project/projectRouter');
+
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Server get!</h2>`)
